0695d626a43206ddc9ef8cd76eb366572d5f37b2
Rangel Paolo <paolorangel1996@outlook.com>
2022-08-16 22:36:34 -0300

    Obtaining post by id

---
+++ src/main/java/com/rangel/workshopmongo/services/PostService.java
@@ -0,0 +1,26 @@
+package com.rangel.workshopmongo.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.rangel.workshopmongo.domain.Post;
+import com.rangel.workshopmongo.repository.PostRepository;
+import com.rangel.workshopmongo.services.exceptions.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repository;
+
+    public Post findById(String id) {
+        Optional<Post> post = repository.findById(id);
+        if (post.get() == null) {
+            throw new ObjectNotFoundException("Object not found");
+        }
+        return post.get();
+    }
+}
