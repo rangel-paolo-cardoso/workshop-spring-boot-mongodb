5bf146c0dc8e7069cc7faf0ee52a5bb50eb2ec8f
Rangel Paolo <paolorangel1996@outlook.com>
2022-08-18 21:00:12 -0300

    Adding comments to the posts

--- src/main/java/com/rangel/workshopmongo/resources/PostResource.java
+++ src/main/java/com/rangel/workshopmongo/resources/PostResource.java
@@ -1,20 +1,12 @@
 package com.rangel.workshopmongo.resources;
 
-import java.net.URI;
-import java.util.List;
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.rangel.workshopmongo.domain.Post;
 import com.rangel.workshopmongo.domain.Post;
 import com.rangel.workshopmongo.services.PostService;
 
--- src/main/java/com/rangel/workshopmongo/services/PostService.java
+++ src/main/java/com/rangel/workshopmongo/services/PostService.java
@@ -1,6 +1,5 @@
 package com.rangel.workshopmongo.services;
 
-import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
